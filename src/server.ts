f0060b61e24247d314127aef19a911fac29ac583
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-02-24 18:08:45 -0300

    Importing ts files

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) =>
-  response.json({ message: "Hello GoStack " })
-);
+app.use(routes);
 
 app.listen(5555, () => {
   console.log("🚀 Server Started on port 5555!");
