aa3c509dcf115b0079e4f72c808af0d553c20d32
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-02-25 17:04:25 -0300

    TypeORM configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes';
 
-const app = express();
-app.use(express.json());
+import './database';
 
+const app = express();
+
+app.use(express.json());
 app.use(routes);
 
-app.listen(5555, () => {
-  console.log('🚀 Server Started on port 5555!');
+app.listen(3333, () => {
+  console.log('🚀 Server Started on port 3333!');
 });
