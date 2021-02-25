86466d076a6cd0dfb826f4bc1e10fbf32f149410
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-02-25 15:29:24 -0300

    Debug Teste

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
 
 app.use(routes);
 
 app.listen(5555, () => {
-  console.log("🚀 Server Started on port 5555!");
+  console.log('🚀 Server Started on port 5555!');
 });
