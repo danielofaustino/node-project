17379b03d113df12437e8fe090116f8c9ebdc9c9
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-02-26 16:47:10 -0300

    Models relationships

--- src/database/migrations/1614360093503-CreateUsers.ts
+++ src/database/migrations/1614360093503-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1614360093503 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default:'uuid_generate_v4()',
